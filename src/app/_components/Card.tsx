3c265d954a099e38adf391977101f11172614065
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-03-16 02:03:56 +0900

    スマホのスタイル調整

--- src/app/_components/Card.tsx
+++ src/app/_components/Card.tsx
@@ -1,3 +1,5 @@
 export const Card = ({ children, className }: { children: React.ReactNode; className: string }) => {
-  return <div className={`${className} bg-white sm:shadow-lg sm:rounded-lg p-6`}>{children}</div>;
+  return (
+    <div className={`${className} bg-white sm:shadow-lg sm:rounded-lg p-3 sm:p-6`}>{children}</div>
+  );
 };
