7a308fedf7371c47887a535989aa4c90f5cb4ff2
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-03-16 03:18:55 +0900

    スタイル修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1 className='text-center text-5xl font-bold my-4'>Image Data</h1>
-      <div className='mx-0 my-0 sm:my-4 lg:mx-auto lg:w-[992px]'>
+      <div className='mx-0 my-0 sm:mx-4 sm:my-4 lg:mx-auto lg:w-[992px]'>
         <Markdown />
       </div>
     </main>
